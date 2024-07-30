793181dd001a910748391eb01724cb9920c1b33e
Builder34 <lcbiao34@gmail.com>
2024-07-30 09:53:45 +0800

    update mirrors

--- k8s/deploy/setup-cluster.sh
+++ k8s/deploy/setup-cluster.sh
@@ -6,8 +6,8 @@ helm repo add postgres-operator-charts https://opensource.zalando.com/postgres-o
 helm repo add strimzi https://strimzi.io/charts/
 helm repo add akhq https://akhq.io/
 helm repo add elastic https://helm.elastic.co
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo add grafana "https://helm-charts.itboon.top/grafana" --force-update
+helm repo add prometheus-community "https://helm-charts.itboon.top/prometheus-community" --force-update
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
